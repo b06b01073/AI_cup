ab39cfdfeb3646bb1c7721f9da45db5de14ad7ec
b06b01073 <b06b01073@gmail.com>
2023-11-18 20:08:59 +0800

    stacked style tta

--- style_collector.py
+++ style_collector.py
@@ -8,16 +8,19 @@ from tqdm import tqdm
 import torch
 import numpy as np
 import os
+import torch.nn.functional as F
+import gogame
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def predict(net, game_feature, tta):
     with torch.no_grad():
-        last_position = torch.from_numpy(game_feature).unsqueeze(dim=0).to(device)
-
-
         if tta:
-            pred = goutils.test_time_style(last_position, net, device)
+            sym_games = np.array(gogame.all_symmetries(game_feature))
+            last_position = torch.tensor(sym_games).to(device)
+            pred = net(last_position)
+            pred = F.softmax(pred, dim=1)
+            pred = torch.mean(pred, dim=0)
         else:
             pred = net(last_position).squeeze()
             pred = torch.softmax(pred, dim=0)
@@ -46,8 +49,8 @@ def fetch_features(games):
 
             go_env.make_move(go_move)
             last_move = move[0]
-        game_features.append(goutils.pad_board(go_env.game_features()))
-    return game_features
+        game_features.append(goutils.crop_move_as_center(go_env.game_features()))
+    return np.array(game_features)
 
 if __name__ == '__main__':
 
@@ -58,9 +61,14 @@ if __name__ == '__main__':
     parser.add_argument('--output', type=str)
 
     args = parser.parse_args()
-    ensemble_preds = None
+    ensemble_preds = 0
     games, file_names = GoParser.file_test_parser(args.test_file)
-    game_features = fetch_features(games)
+
+    # run this block for the first time
+    # game_features = fetch_features(games) 
+    # np.save('dataset/testing/play_style.npy', game_features)
+    
+    game_features = np.load('dataset/testing/play_style.npy')
 
     with open(args.output, 'w') as f:
        
@@ -71,9 +79,6 @@ if __name__ == '__main__':
             preds = []
             for feature in tqdm(game_features, dynamic_ncols=True):
                 preds.append(predict(net, feature, args.tta))
-
-            if ensemble_preds is None:
-                ensemble_preds = torch.zeros((len(preds), govars.STYLE_CAT)).to(device)
 
             ensemble_preds += torch.stack(preds)
 
